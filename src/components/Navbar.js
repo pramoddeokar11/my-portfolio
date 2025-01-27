081ccd58d23d5b705160c632235fb3f703db0864
Pramod Deokar <pramoddeokar115@gmail.com>
2025-01-28 00:31:12 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobile(!isMobile);
+  };
+
   return (
     <nav className="navbar">
       <h1 className="logo">My Portfolio</h1>
-      <ul className="nav-links">
+      <ul className={`nav-links ${isMobile ? 'mobile' : ''}`}>
         <li><a href="#hero">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#education">Education</a></li>
@@ -13,6 +19,11 @@ const Navbar = () => {
         <li><a href="#testimonials">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
+      <div className="hamburger" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
     </nav>
   );
 };
