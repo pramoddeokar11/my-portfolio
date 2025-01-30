97142b66043356a53fffa6d2008f76e43308090a
Pramod Deokar <pramoddeokar115@gmail.com>
2025-01-30 17:40:33 +0530

    reduced about section

--- src/components/About.js
+++ src/components/About.js
@@ -12,8 +12,7 @@ const About = () => {
           With expertise in both frontend and backend technologies, I specialize in creating responsive, scalable, and efficient solutions tailored to meet client needs.
           I have hands-on experience working with modern technologies like JavaScript, React.js, Node.js, Express.js, and MongoDB, enabling me to deliver seamless end-to-end development. 
           I am also skilled in API development, system optimization, and ensuring robust application security.
-          Driven by a love for problem-solving and continuous learning, I enjoy turning ideas into reality through code. 
-          Whether it's crafting pixel-perfect designs or building complex backend systems, I take pride in delivering high-quality solutions that exceed expectations.
+          Driven by a love for problem-solving and continuous learning, I enjoy turning ideas into reality through code.
           </p>
         </div>
       </div>
